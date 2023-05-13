9aeb171c76da32118e69c6f7a14e6ee63d8bf208
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-13 14:03:44 -0700

    added if stmt for breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,9 +5,12 @@ const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 
 request(url, (error, response, body) => {
-  console.log(typeof body);
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-  console.log(typeof data);
 
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("Cat breed not found, sorry!");
+  } else {
+    console.log(data[0].description);
+  }
+  
 });
